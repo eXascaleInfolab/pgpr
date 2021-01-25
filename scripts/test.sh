208738985b7a400034735997ed399539d93ba826
Ines Arous <ines@exascale.info>
2021-01-25 09:32:54 +0100

    deleting previous version of pgpr

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-python3 ../src/variational_inference.py \
-		--inputfile_reviews  '../input/labeled_data.csv'\
-		--answer_matrix '../input/answer_matrix.csv'\
-		--sup_rate 6\
-		--iterr 10\
-		--classifier 'svm'\
-		--evaluation_file '../output/small_example.csv'\
